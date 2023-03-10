8a0ba6778f03e72210a16bc0fe6c83bf9ed46269
Kirk Baker <kbaker@camerabits.com>
2023-03-09 18:40:55 -0800

    Now copies libs/headers out to ../lib and ../include

--- build_mac_universal.sh
+++ build_mac_universal.sh
@@ -23,7 +23,12 @@ mkdir build_mac_universal
 mkdir build_mac_universal/Debug
 mkdir build_mac_universal/Release
 
-QT_PREFIX_PATH="/Users/kbaker/Development/Qt/6.3.2/macos"
+if [[ ! -d "$QT_PREFIX_PATH" ]]; then
+	echo "QT_PREFIX_PATH has not been set or is not a valid path."
+	echo "QT_PREFIX_PATH=$QT_PREFIX_PATH"
+	echo "Add the QT_PREFIX_PATH environment variable pointing to your Qt platform installation."
+	exit 1
+fi
 
 cd build_Arm64/Debug
 cmake -DCMAKE_PREFIX_PATH=$QT_PREFIX_PATH -DCMAKE_BUILD_TYPE=Debug -DCMAKE_OSX_ARCHITECTURES=arm64 -G"Unix Makefiles" ../../CMakeLists.txt
@@ -50,12 +55,10 @@ make clean
 cd ../../build_mac_universal
 lipo -create ../build_Arm64/Debug/libqtadvanceddocking.dylib ../build_x86_64/Debug/libqtadvanceddocking.dylib -output Debug/libqtadvanceddocking.dylib
 cd Debug
-ln -s libqtadvanceddocking.dylib libqtadvanceddocking.3.8.2.dylib
 cd ..
 echo "Universal Debug dylib created" >&2
 lipo -create ../build_Arm64/Release/libqtadvanceddocking.dylib ../build_x86_64/Release/libqtadvanceddocking.dylib -output Release/libqtadvanceddocking.dylib
 cd Release
-ln -s libqtadvanceddocking.dylib libqtadvanceddocking.3.8.2.dylib
 install_name_tool -delete_rpath $QT_PREFIX_PATH/lib libqtadvanceddocking.dylib -add_rpath "@executable_path/../Frameworks"
 cd ..
 echo "Universal Release dylib created" >&2
@@ -67,6 +70,39 @@ rm -rf ../build_x86_64
 file Debug/libqtadvanceddocking.dylib
 file Release/libqtadvanceddocking.dylib
 
-# need to copy the libs and headers
+# copy the libs
+cp Debug/libqtadvanceddocking.dylib ../../lib/Debug/bin
+cp Release/libqtadvanceddocking.dylib ../../lib/Release/bin
+# setup symlinks
+ln -s libqtadvanceddocking.dylib ../../lib/Debug/bin/libqtadvanceddocking.3.8.2.dylib
+ln -s libqtadvanceddocking.dylib ../../lib/Release/bin/libqtadvanceddocking.3.8.2.dylib
+
+# copy the headers
+cd ../src
+rm -rf ../../include/ads
+mkdir ../../include/ads
+cp AutoHideDockContainer.h ../../include/ads/
+cp AutoHideSideBar.h ../../include/ads/
+cp AutoHideTab.h ../../include/ads/
+cp DockAreaTabBar.h ../../include/ads/
+cp DockAreaTitleBar.h ../../include/ads/
+cp DockAreaTitleBar_p.h ../../include/ads/
+cp DockAreaWidget.h ../../include/ads/
+cp DockComponentsFactory.h ../../include/ads/
+cp DockContainerWidget.h ../../include/ads/
+cp DockFocusController.h ../../include/ads/
+cp DockManager.h ../../include/ads/
+cp DockOverlay.h ../../include/ads/
+cp DockSplitter.h ../../include/ads/
+cp DockWidget.h ../../include/ads/
+cp DockWidgetTab.h ../../include/ads/
+cp DockingStateReader.h ../../include/ads/
+cp ElidingLabel.h ../../include/ads/
+cp FloatingDockContainer.h ../../include/ads/
+cp FloatingDragPreview.h ../../include/ads/
+cp IconProvider.h ../../include/ads/
+cp PushButton.h ../../include/ads/
+cp ResizeHandle.h ../../include/ads/
+cp ads_globals.h ../../include/ads/
 
 echo "done" >&2
